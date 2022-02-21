abdc234a6b457d2ce70aecf7db7118bb9db73664
Sethu Gopalan <SethuGopalan@outlook.com>
2022-02-21 15:58:48 -0500

    update about

--- application.py
+++ application.py
@@ -10,6 +10,10 @@ def index():
 def contact():
     return render_template('contact.html', title='Contact')
 
+@application.route("/about")
+def contact():
+    return render_template('about.html', title='About')
+
 if __name__ == '__main__':
     application.debug = True
     application.run()
