c6c3937b5ca6ef2373d232473a3dff9477e6ca21
Sethu Gopalan <SethuGopalan@outlook.com>
2022-02-21 16:02:50 -0500

    about page update fuction

--- application.py
+++ application.py
@@ -11,7 +11,7 @@ def contact():
     return render_template('contact.html', title='Contact')
 
 @application.route("/about")
-def contact():
+def about():
     return render_template('about.html', title='About')
 
 if __name__ == '__main__':
